e47723a1ce18618fcf0abeab2bc91be21b10c4cd
unknown <robinkaris7@gmail.com>
2021-11-10 21:35:04 +0300

    made changes

--- controllers/dbDatacontroller.js
+++ controllers/dbDatacontroller.js
@@ -32,9 +32,11 @@ const getDbContacts = (req,res) =>{
     const sqlConn = new sql.ConnectionPool(config);
     sqlConn.connect().then(function() {
         const request = new sql.Request(sqlConn);
-        const store_ID = req.body.store_ID
+        const store_ID = Object.keys(req.body).pop(); 
+        
+        // console.log(store_ID)
     
-       request.query("select contact from contacts where [store ID]='"+store_ID+"'")
+       request.query("select * from contacts where [store ID]='"+store_ID+"'")
        .then(function (recordsets) {
         sqlConn.close();
         return res.send(recordsets)
